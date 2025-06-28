b9b2207aa6eedd4ca7d7828a6865f36e6aeb64ee
Srinjoy Sen Chowdhury <116475469+AllMightLegend@users.noreply.github.com>
2025-06-28 14:05:38 +0530

    Update CalculatorTest.java

--- Week2_TDD using JUnit5 and Mockito/JUnit/Exercise 1_Setting Up JUnit/Code/CalculatorTest.java
+++ Week2_TDD using JUnit5 and Mockito/JUnit/Exercise 1_Setting Up JUnit/Code/CalculatorTest.java
@@ -8,7 +8,7 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         int result = 2 + 3;
-        System.out.println("Adding 2 + 3 = " + result);  // 👈 This will print
+        System.out.println("Adding 2 + 3 = " + result);  // This will print
         assertEquals(5, result);
     }
 }
